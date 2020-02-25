82e97c067d5b04e3c6e0ab862487254c98e499ac
Stephan Krings <sk@interkiez.berlin>
2020-02-25 13:45:16 -0800

    Add file with required env vars and their meaning

---
+++ environment.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SF_ORG_URL=<URL of the Salesforce Org>
+export SF_USERNAME=<Salesforce Username for API calls>
+export SF_PASSWORD=<Password of above user>
+export SF_SECURITY_TOKEN=<Security Token for that user>
+export SF_CLIENT_ID=<Salesforce Client ID (Consumer Key) for Connected App>
+export SF_CLIENT_SECRET=<Salesforce Client Secret (Consumer Secret) for Connected App>
+export NEXMO_WHATSAPP_AUTH_TOKEN=<Authentication Key for Nexmo>
+export NEXMO_WHATSAPP_PHONE=<Whatsapp Phone Number for Nexmo>
